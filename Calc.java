1332af6ac33907466bd645a4ab54b6a62f7d84b3
Doglas <doglasseonhak3@gmail.com>
2015-11-19 13:02:23 +1100

    Test Commit for Calc.java

---
+++ Calc.java
@@ -0,0 +1,15 @@
+package com.example.ms_win.unittest;
+
+/**
+ * Created by MS_WIN on 2015-11-17.
+ */
+public abstract  class Calc
+{
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+    public abstract int multiply(int a, int b);
+    public abstract int multiply2(int a, int b);
+}
+}
