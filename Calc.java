0be27b7cbef6ad2c4c2cd754429fa2dc3aceecdc
Doglas <doglasseonhak3@gmail.com>
2015-11-20 18:27:49 +1100

    Add Conflict Tool1

--- Calc.java
+++ Calc.java
@@ -25,6 +25,8 @@ public abstract  class Calc
 
 	public abstract int ConflictMethod(int a, int b);
 	public abstract int ConflictMethod2(int a, int b);
+
+	public abstract int ConflictTool1(int a, int b);
 }
 
 // Add Sentence1
